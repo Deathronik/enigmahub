19da1da0e99946457d7063b2111dbffef77c97e7
Deathronik <bemavadim@gmail.com>
2024-01-25 19:15:32 +0200

    Change mobile detection method

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,34 @@ import './App.css'
 import Sidebar from "./components/Sidebar/Sidebar.tsx";
 
 import {useRoutes} from "./hooks/useRouter.tsx";
-import {useEffect, useState} from "react";
+import { BrowserView, MobileView} from 'react-device-detect';
 
 const App = () => {
     const routes = useRoutes()
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024)
-
-    useEffect(() => {
-        setIsMobile(window.innerWidth <= 1024)
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 1024)
-        }
-
-        window.addEventListener('resize', handleResize)
-
-        return () => window.removeEventListener('resize', handleResize)
-    }, [])
-
 
     return (
         <div className="p-2 flex flex-row fadeIn">
-            {isMobile && <div className="drawer">
-                <input id="my-drawer" type="checkbox" className="drawer-toggle"/>
-                <div className="drawer-content">
-                    <label htmlFor="my-drawer" className="btn drawer-button">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                             className="inline-block w-5 h-5 stroke-current">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                  d="M4 6h16M4 12h16M4 18h16"></path>
-                        </svg>
-                    </label>
+            <MobileView>
+                <div className="drawer">
+                    <input id="my-drawer" type="checkbox" className="drawer-toggle"/>
+                    <div className="drawer-content">
+                        <label htmlFor="my-drawer" className="btn drawer-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 className="inline-block w-5 h-5 stroke-current">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                      d="M4 6h16M4 12h16M4 18h16"></path>
+                            </svg>
+                        </label>
+                    </div>
+                    <div className="drawer-side z-[2]">
+                        <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                        <Sidebar/>
+                    </div>
                 </div>
-                <div className="drawer-side z-[2]">
-                    <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-                    <Sidebar/>
-                </div>
-            </div>}
-            {!isMobile && (<Sidebar/>)}
+            </MobileView>
+            <BrowserView>
+                <Sidebar/>
+            </BrowserView>
             <div className="flex-grow flex justify-center items-center ml-5">
                 {routes}
             </div>
