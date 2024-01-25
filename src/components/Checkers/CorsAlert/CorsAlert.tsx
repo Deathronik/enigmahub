612699405676dee65b66a51ad3c1cd6a24ae0c9a
Deathronik <bemavadim@gmail.com>
2024-01-25 20:44:45 +0200

    Add fadeIn animation for Alerts

--- src/components/Checkers/CorsAlert/CorsAlert.tsx
+++ src/components/Checkers/CorsAlert/CorsAlert.tsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 
 const CorsAlert = () => {
     return (
-        <div role="alert" className="alert alert-error w-96">
+        <div role="alert" className="alert alert-error w-96 fadeIn">
             <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none"
                  viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
