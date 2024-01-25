9cc410faa33eb2b0358fd14e0bc61c6d6926c3fc
Deathronik <bemavadim@gmail.com>
2024-01-25 14:44:48 +0200

    Moved imgs to public directory

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,9 +5,9 @@ const Home = () => {
         <div className="flex flex-col justify-center items-center">
             <h1 className="font-bold text-4xl">You can check now</h1>
             <div className="flex flex-row mt-3">
-                <Card title={"$ZETA Airdrop"} text={"Zetachain"} imgSrc={"/src/assets/imgs/airdrop2.png"} link={"/check/zeta"} isAirdrop={true}/>
-                <Card title={"$DYM Airdrop"} text={"Dymension"} imgSrc={"/src/assets/imgs/airdrop2.png"} link={"/check/dym"} isAirdrop={true}/>
-                <Card title={"$JUP Airdrop"} text={"Jupiter"} imgSrc={"/src/assets/imgs/airdrop2.png"} link={"/check/jup"} isAirdrop={true}/>
+                <Card title={"$ZETA Airdrop"} text={"Zetachain"} imgSrc={"/assets/imgs/airdrop2.png"} link={"/check/zeta"} isAirdrop={true}/>
+                <Card title={"$DYM Airdrop"} text={"Dymension"} imgSrc={"/assets/imgs/airdrop2.png"} link={"/check/dym"} isAirdrop={true}/>
+                <Card title={"$JUP Airdrop"} text={"Jupiter"} imgSrc={"/assets/imgs/airdrop2.png"} link={"/check/jup"} isAirdrop={true}/>
             </div>
         </div>
     )
