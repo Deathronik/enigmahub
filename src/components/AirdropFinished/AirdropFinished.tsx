8cb064f12f61bee993802958ae6ba97acbe82785
Deathronik <bemavadim@gmail.com>
2024-02-01 18:02:12 +0200

    Added an margin top for the airdrop finished component to eliminate drawer overlaying issues

--- src/components/AirdropFinished/AirdropFinished.tsx
+++ src/components/AirdropFinished/AirdropFinished.tsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 
 const AirdropFinished = ({stage}: {stage: string}) => {
     return (
-        <div className="flex flex-col justify-center items-center font-bold text-center">
+        <div className="flex flex-col justify-center items-center font-bold text-center mt-12">
             <h1 className="text-2xl">If you see this page, it means the {stage} is complete</h1>
             <img src="/assets/imgs/airdrop.png" alt="airdrop" className="mt-4 mb-4 rounded-box w-2/5"/>
             <p>You can check any of the other available airdrops</p>
