7b436e99cf94b96e02b06f94c2772ec46b9f2d93
Deathronik <bemavadim@gmail.com>
2024-02-02 15:57:40 +0200

    Fixed shadow in Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 
 const Card = ({title, text, link, imgSrc, isAirdrop}: { title: string, text: string, link: string, imgSrc: string, isAirdrop: boolean }) => {
     return (
-        <div className={`pt-5 m-2 card bg-base-200 shadow-xl flex flex-col justify-center items-center fadeIn ${isAirdrop ? "w-64" : "w-96"}`}>
+        <div className={`pt-5 m-2 card bg-base-200 shadow-md flex flex-col justify-center items-center fadeIn ${isAirdrop ? "w-64" : "w-96"}`}>
             {imgSrc && <img src={imgSrc} alt={imgSrc} className="p-2 w-52 h-52"/>}
             <div className="card-body items-center text-center pt-2 pb-5">
                 <h2 className="card-title">{title}</h2>
