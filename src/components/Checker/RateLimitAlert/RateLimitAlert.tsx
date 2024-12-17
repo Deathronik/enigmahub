cf3db8428a05c96b134fbf44330d571f88b86a91
Deathronik <bemavadim@gmail.com>
2024-12-17 16:51:33 +0200

    Updated RateLimit alert info

--- src/components/Checker/RateLimitAlert/RateLimitAlert.tsx
+++ src/components/Checker/RateLimitAlert/RateLimitAlert.tsx
@@ -6,7 +6,7 @@ const RateLimitAlert = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                       d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"/>
             </svg>
-            <span>To avoid project API rate limits, check wallets in batches of 15 or less. If that doesn't help, open a new tab in incognito mode and try again. Or just wait (though it may take longer)</span>
+            <span>To avoid project API rate limits, check wallets in batches of 10 or less. If that doesn't help, open a new tab in incognito mode or VPN and try again. Or just wait (though it may take longer)</span>
         </div>
     )
 }
